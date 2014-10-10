415b2601ab80c07a3898c6cf9a1f47610d128d31
Phillip Shipley <phillip_shipley@sil.org>
2014-10-10 09:11:02 -0400

    added pear channel update to php::xdebug

--- php/recipes/xdebug.rb
+++ php/recipes/xdebug.rb
@@ -6,6 +6,11 @@
 	end
 end
 
+# Update pecl channel
+php_pear_channel "pecl.php.net" do
+    action :update
+end
+
 # install the xdebug pecl
 php_pear "xdebug" do
   # Specify that xdebug.so must be loaded as a zend extension
