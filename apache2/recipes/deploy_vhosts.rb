aa4ce5746724b8f8c9cf6658cceca3d71a970e0b
Phillip Shipley <phillip_shipley@sil.org>
2014-10-10 14:56:15 -0400

    update deploy_vhosts

--- apache2/recipes/deploy_vhosts.rb
+++ apache2/recipes/deploy_vhosts.rb
@@ -4,29 +4,31 @@
 
 node['deploy'].each do |appname, deploy|
     # Put SSL cert files where they go
-    file "#{deploy['vhost']['ssl']['cert_path']}/#{deploy['vhost']['server_name']}.crt" do
-        owner "root"
-        group "root"
-        mode "0644"
-        action :create
-        content deploy['vhost']['ssl']['cert_content']
-        only_if { deploy['vhost']['ssl'] && deploy['vhost']['ssl']['enabled'] && !File.exists?("#{deploy['vhost']['ssl']['cert_path']}/#{deploy['vhost']['server_name']}.crt") }
-    end
-    file "#{deploy['vhost']['ssl']['key_path']}/#{deploy['vhost']['server_name']}.key" do
-        owner "root"
-        group "root"
-        mode "0600"
-        action :create
-        content deploy['vhost']['ssl']['key_content']
-        only_if { deploy['vhost']['ssl'] && deploy['vhost']['ssl']['enabled'] && !File.exists?("#{deploy['vhost']['ssl']['key_path']}/#{deploy['vhost']['server_name']}.key") }
-    end
-    file "#{deploy['vhost']['ssl']['intermediate_cert_file']}" do
-        owner "root"
-        group "root"
-        mode "0644"
-        action :create
-        content deploy['vhost']['ssl']['intermediate_cert_content']
-        only_if { deploy['vhost']['ssl'] && deploy['vhost']['ssl']['enabled'] && deploy['vhost']['ssl']['intermediate_cert_file'] && !File.exists?(deploy['vhost']['ssl']['intermediate_cert_file']) }
+    if deploy['vhost']['ssl']['enabled']
+        file "#{deploy['vhost']['ssl']['cert_path']}/#{deploy['vhost']['server_name']}.crt" do
+            owner "root"
+            group "root"
+            mode "0644"
+            action :create
+            content deploy['vhost']['ssl']['cert_content']
+            only_if { deploy['vhost']['ssl'] && deploy['vhost']['ssl']['enabled'] && !File.exists?("#{deploy['vhost']['ssl']['cert_path']}/#{deploy['vhost']['server_name']}.crt") }
+        end
+        file "#{deploy['vhost']['ssl']['key_path']}/#{deploy['vhost']['server_name']}.key" do
+            owner "root"
+            group "root"
+            mode "0600"
+            action :create
+            content deploy['vhost']['ssl']['key_content']
+            only_if { deploy['vhost']['ssl'] && deploy['vhost']['ssl']['enabled'] && !File.exists?("#{deploy['vhost']['ssl']['key_path']}/#{deploy['vhost']['server_name']}.key") }
+        end
+        file "#{deploy['vhost']['ssl']['intermediate_cert_file']}" do
+            owner "root"
+            group "root"
+            mode "0644"
+            action :create
+            content deploy['vhost']['ssl']['intermediate_cert_content']
+            only_if { deploy['vhost']['ssl'] && deploy['vhost']['ssl']['enabled'] && deploy['vhost']['ssl']['intermediate_cert_file'] && !File.exists?(deploy['vhost']['ssl']['intermediate_cert_file']) }
+        end
     end
 
     # Create vhost 
