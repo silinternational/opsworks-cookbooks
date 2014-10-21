0dcff5e2c155def7525ae5eefcc3d950e034c7aa
Phillip Shipley <phillip_shipley@sil.org>
2014-10-21 15:18:14 -0400

    adding check to yii_migrate for yii_dir in config

--- php/recipes/yii_migrate.rb
+++ php/recipes/yii_migrate.rb
@@ -1,7 +1,9 @@
 # Run yii migrations
 node['deploy'].each do |appname, deploy|
-    yii_migrate "yii_migrate" do
-        path "#{deploy['deploy_to']}#{deploy['aws_extra_path']}/#{deploy['yii_dir']}"
-        only_if { File.exists?("#{deploy['deploy_to']}#{deploy['aws_extra_path']}/#{deploy['yii_dir']}/yiic") }
+    if deploy['yii_dir']
+        yii_migrate "yii_migrate" do
+            path "#{deploy['deploy_to']}#{deploy['aws_extra_path']}/#{deploy['yii_dir']}"
+            only_if { File.exists?("#{deploy['deploy_to']}#{deploy['aws_extra_path']}/#{deploy['yii_dir']}/yiic") }
+        end
     end
 end
