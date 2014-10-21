4c54528e88d7a31ecf13fbe1e3eba3f95f43876b
Phillip Shipley <phillip_shipley@sil.org>
2014-10-21 09:00:05 -0400

    updating path used in yii_migrate

--- php/recipes/yii_migrate.rb
+++ php/recipes/yii_migrate.rb
@@ -1,6 +1,6 @@
 # Run yii migrations
 node['deploy'].each do |appname, deploy|
     yii_migrate "yii_migrate" do
-        path "#{deploy['deploy_to']}/#{deploy['yii_dir']}"
+        path "#{deploy['deploy_to']}#{deploy['aws_extra_path']}/#{deploy['yii_dir']}"
     end
 end
