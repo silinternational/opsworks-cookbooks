f53f86b33630c26b987835cf0c126f5db67b5def
Phillip Shipley <phillip_shipley@sil.org>
2014-10-21 16:06:05 -0400

    update addressbook dependencies

--- addressbook/metadata.rb
+++ addressbook/metadata.rb
@@ -5,8 +5,3 @@ version "0.0.1"
 maintainer "SIL International, GTIS"
 
 recipe "addressbook::default", "Main application configuration and core dependencies"
-
-depends "apache2"
-depends "silphp"
-depends "silapache2"
-depends "simplesamlphp"
