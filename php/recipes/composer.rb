b03a88ab64639ac28ebc9675e2d4c05f39c2b8d4
Phillip Shipley <phillip_shipley@sil.org>
2014-10-21 15:08:51 -0400

    updated composer and yii_migrate recipes to be conditional

--- php/recipes/composer.rb
+++ php/recipes/composer.rb
@@ -5,6 +5,7 @@ node['deploy'].each do |appname, deploy|
             path "#{deploy['deploy_to']}#{deploy['aws_extra_path']}/#{deploy['composer']['dir']}"
             self_update deploy['composer']['self_update']
             as_update deploy['composer']['as_update']
+            only_if { File.exists?("#{deploy['deploy_to']}#{deploy['aws_extra_path']}/#{deploy['composer']['dir']}/composer.json") }
         end
     end
 end
--- php/recipes/yii_migrate.rb
+++ php/recipes/yii_migrate.rb
@@ -2,5 +2,6 @@
 node['deploy'].each do |appname, deploy|
     yii_migrate "yii_migrate" do
         path "#{deploy['deploy_to']}#{deploy['aws_extra_path']}/#{deploy['yii_dir']}"
+        only_if { File.exists?("#{deploy['deploy_to']}#{deploy['aws_extra_path']}/#{deploy['yii_dir']}/yiic") }
     end
 end
