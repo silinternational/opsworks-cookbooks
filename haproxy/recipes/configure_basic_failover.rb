cddcba1fa0bf59cdfb85c335f86fda69369e939f
Phillip Shipley <phillip_shipley@sil.org>
2014-11-12 15:11:56 -0500

    update haproxy recipe

--- haproxy/recipes/configure_basic_failover.rb
+++ haproxy/recipes/configure_basic_failover.rb
@@ -1,3 +1,7 @@
+package 'haproxy' do
+  action :install
+end
+
 service "haproxy" do
   supports :restart => true, :status => true, :reload => true
   action :nothing # only define so that it can be restarted if the config changed
