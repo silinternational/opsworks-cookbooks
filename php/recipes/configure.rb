f3053d1d3e8bad6a96db7bee6136cb7f1000862f
Phillip Shipley <phillip_shipley@sil.org>
2015-02-02 13:50:36 -0500

    updated php::configure to support unique paths for different config files

--- php/recipes/configure.rb
+++ php/recipes/configure.rb
@@ -4,7 +4,11 @@ node['deploy'].each do |application, deploy|
     deploy['config_files'].each do |file, data|
       template "config file #{data['filename']}" do
         source 'array.php.erb'
-        path "#{deploy['deploy_to']}#{deploy['aws_extra_path']}/#{deploy['config_path']}/#{data['filename']}"
+        if data['path']
+          path "#{deploy['deploy_to']}#{deploy['aws_extra_path']}/#{data['path']}/#{data['filename']}"
+        else
+          path "#{deploy['deploy_to']}#{deploy['aws_extra_path']}/#{deploy['config_path']}/#{data['filename']}"
+        end
         mode '0664'
         owner deploy['owner']
         group deploy['group']
