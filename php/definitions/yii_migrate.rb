3ba3f2f8c153658009c1d1795127b3c84368ea73
Phillip Shipley <phillip_shipley@sil.org>
2015-02-02 13:49:34 -0500

    Updated php::yii_migrate and yii_migrate to support yii_ver to determine which command format to use

--- php/definitions/yii_migrate.rb
+++ php/definitions/yii_migrate.rb
@@ -2,7 +2,16 @@ define :yii_migrate do
 	if params[:path]
 		execute "Running yii migrations in #{params[:path]}" do
 			user "root"
-			command "#{params[:path]}/yiic migrate --interactive=0"
+            if params[:dbname]
+                dbname = params[:dbname]
+            else
+                dbname = "db"
+            end
+            if params[:yii_ver] == 2
+                command "#{params[:path]}/yii migrate --interactive=0 --db=#{dbname}"
+            else
+			    command "#{params[:path]}/yiic migrate --interactive=0 --connectionID=#{dbname}"
+            end
 		end
 	end
 end
--- php/recipes/yii_migrate.rb
+++ php/recipes/yii_migrate.rb
@@ -2,8 +2,12 @@
 node['deploy'].each do |appname, deploy|
     if deploy['yii_dir']
         yii_migrate "yii_migrate" do
+            if deploy['yii_ver']
+                yii_ver deploy['yii_ver']
+            else
+                yii_ver 1
+            end
             path "#{deploy['deploy_to']}#{deploy['aws_extra_path']}/#{deploy['yii_dir']}"
-            only_if { File.exists?("#{deploy['deploy_to']}#{deploy['aws_extra_path']}/#{deploy['yii_dir']}/yiic") }
         end
     end
 end
