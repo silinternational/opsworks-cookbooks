ab0dd434cc3db106b42f6990a1542716c80a44f5
Phillip Shipley <phillip_shipley@sil.org>
2015-03-17 16:05:24 -0400

    updated doorman recipe

--- doorman/recipes/default.rb
+++ doorman/recipes/default.rb
@@ -3,11 +3,11 @@
 # Required packages
 case node[:platform_family]
   when 'rhel'
-    packages = ["git", "httpd", "php", "php-mcrypt", "php-xml", "php-mbstring", "php-pdo", "php-mysql", "nodejs", "npm"]
+    packages = ["git", "httpd", "php", "php-mcrypt", "php-xml", "php-mbstring", "php-pdo", "php-mysql"]
     apache_owner = "apache"
     apache_group = "apache"
   when 'debian'
-    packages = ["git", "apache2", "php5", "php5-cli", "php5-mcrypt", "php5-mysql", "ruby-dev", "npm"]
+    packages = ["git", "apache2", "php5", "php5-cli", "php5-mcrypt", "php5-mysql"]
     apache_owner = "www-data"
     apache_group = "www-data"
   end
@@ -35,9 +35,8 @@ folders.each do |folder|
   directory "#{api['deploy_to']}#{api['aws_extra_path']}#{folder}" do
     owner apache_owner
     group apache_group
-    group "apache"
     mode "0775"
-    only_if { File.directory?("#{api['deploy_to']}#{api['aws_extra_path']}#{folder}") }
+    #only_if { File.directory?("#{api['deploy_to']}#{api['aws_extra_path']}#{folder}") }
   end
 end
 
